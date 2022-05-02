e37907a16d84d842e7d0f743d7e1056bea9caf31
Jiajie Chen <c@jia.je>
2022-05-02 20:17:35 +0800

    Fix submodules script

--- submodules.sh
+++ submodules.sh
@@ -4,10 +4,11 @@ git submodule update --init
 cd glibc
 git remote add loongson git@github.com:loongson/glibc.git
 git fetch loongson
+cd ..
 
 cd linux
 git remote add loongson git@github.com:loongson/linux.git
 git fetch loongson
-
 cd ..
+
 git submodule update --init
