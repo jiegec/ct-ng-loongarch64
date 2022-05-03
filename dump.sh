f6d027b5ec0f5b31fe1133e72e0c3ec011491277
Jiajie Chen <c@jia.je>
2022-05-03 09:31:27 +0800

    Dump linux version in dump.sh

--- dump.sh
+++ dump.sh
@@ -2,9 +2,15 @@
 TARGET=loongarch64-unknown-linux-gnu
 PREFIX=~/x-tools/$TARGET
 export PATH=$PREFIX/bin:$PATH
+
 which $TARGET-gcc
 $TARGET-gcc --version > dump.log
 $TARGET-ld --version >> dump.log
 $TARGET-gdb --version >> dump.log
+
 cat $PREFIX/$TARGET/sysroot/usr/include/features.h | grep "define\s__GLIBC__" >> dump.log
 cat $PREFIX/$TARGET/sysroot/usr/include/features.h | grep "define\s__GLIBC_MINOR__" >> dump.log
+
+cat $PREFIX/$TARGET/sysroot/usr/include/linux/version.h | grep "LINUX_VERSION_MAJOR" >> dump.log
+cat $PREFIX/$TARGET/sysroot/usr/include/linux/version.h | grep "LINUX_VERSION_PATCHLEVEL" >> dump.log
+cat $PREFIX/$TARGET/sysroot/usr/include/linux/version.h | grep "LINUX_VERSION_SUBLEVEL" >> dump.log
