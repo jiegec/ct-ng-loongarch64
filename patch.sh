245dd79de6336afe7b45da74ee33d3b551526b51
Jiajie Chen <c@jia.je>
2022-05-08 11:21:16 +0800

    Let crosstool-ng clone binutils-gdb, linux and glibc

--- patch.sh
+++ patch.sh
@@ -5,8 +5,3 @@ cd gcc
 git reset HEAD --hard
 git apply ../patches/gcc/001-undo-genmatch-dependency.patch
 cd ..
-
-cd glibc
-git reset HEAD --hard
-git apply ../patches/glibc/001-bump-config-guess-sub.patch
-cd ..
--- submodules.sh
+++ submodules.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
 git submodule update --init
-
-cd glibc
-git remote add loongson git@github.com:loongson/glibc.git
-git fetch loongson
-cd ..
-
-cd linux
-git remote add loongson git@github.com:loongson/linux.git
-git fetch loongson
-cd ..
-
-git submodule update --init
